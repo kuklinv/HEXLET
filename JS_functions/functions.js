3603034b5c0faeedca36f6f3f874dea38f37a69b
Vitaly <vitalykuklin@gmail.com>
2020-02-04 08:40:06 +0700

    Feature: continue functions.js[ref:no-ref]

---
+++ JS_functions/functions.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+// const uniq = require('lodash');
+// import { uniq } from 'lodash';
+
+// const union = (...arrays) => {
+//     let startArr = [];
+//     const conArray = startArr.concat(...arrays);
+//     if (conArray.length === 0) return [];
+//     const resArray = uniq(conArray);
+//     //  return resArray;
+//     console.log(resArray);
+// }
+
+// union([3]); // [3]
+// union([3, 2], [2, 2, 1]); // [3, 2, 1]
+// union(['a', 3, false], [true, false, 3], [false, 5, 8]); // ['a', 3, false, true, 5, 8]
