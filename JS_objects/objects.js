7e9a2e788da50c34776054e7efde57869c8785f8
Vitaly <vitalykuklin@gmail.com>
2020-02-06 07:47:10 +0700

    Feature: continue with objects.js [ref:no-ref]

--- JS_objects/objects.js
+++ JS_objects/objects.js
@@ -1,18 +1,53 @@
 
+
 const data = {
-    "common": {
-        "files": [
-            "src/objects.js"
-        ]
+    user: 'ubuntu',
+    hosts: {
+        0: {
+            name: 'web1',
+        },
+        1: {
+            name: 'web2',
+            null: 3,
+            active: false,
+        },
     },
-    "config": {
-        "outdir": "/dist"
+};
+
+const getIn = (objectInn, findeKaysArray) => {
+    let objectInnkeys = Object.keys(objectInn);
+    console.log(objectInnkeys);
+    for (const key in objectInnkeys) {
+        if (typeof (key) !== 'object') {
+
+            console.log(objectInnkeys[key]);
+        }
     }
 }
 
-const getJsonFileData = (data) => {
-    let jsonObj = JSON.stringify(data, '\t', 2);
-    console.log(jsonObj);
-}
+// getIn(data, ['undefined']); // null
+getIn(data, ['user']); // 'ubuntu'
+  // getIn(data, ['user', 'ubuntu']); // null
+  // getIn(data, ['hosts', 1, 'name']); // 'web2'
+  // getIn(data, ['hosts', 0]); // { name: 'web1' }
+  // getIn(data, ['hosts', 1, null]); // 3
+  // getIn(data, ['hosts', 1, 'active']); // false
 
-getJsonFileData(data);
+
+// const data = {
+//     "common": {
+//         "files": [
+//             "src/objects.js"
+//         ]
+//     },
+//     "config": {
+//         "outdir": "/dist"
+//     }
+// }
+
+// const getJsonFileData = (data) => {
+//     let jsonObj = JSON.stringify(data, '\t', 2);
+//     console.log(jsonObj);
+// }
+
+// getJsonFileData(data);
