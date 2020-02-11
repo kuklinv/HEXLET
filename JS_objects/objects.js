cce1756ca2896fdc9747aa9c49fd39ae84b4ee99
Vitaly <vitalykuklin@gmail.com>
2020-02-11 07:35:33 +0700

    Feature: next version getIn.objects.js [ref: objects.js]

--- JS_objects/objects.js
+++ JS_objects/objects.js
@@ -20,26 +20,21 @@ const data = {
     },
 };
 
-const getIn = (objectInn, findeKaysArray) => {
-    const recTestKeys = key => {
-        for (let i = 0; i <= findeKaysArray.length; i++) {
-            if (key.hasOwnProperty(findeKaysArray[i])) {
-                return objectInn[findeKaysArray[i]];
-                // console.log(objectInn[findeKaysArray[i]]);
-            } else {
-                return null;
-                // console.log('no such key');
-            }
-        }
-        // return objectInn[findeKaysArray[i]];
-    }
-    console.log(findeKaysArray.length);
-    for (let n = 0; n < findeKaysArray.length; n++) {
-        if (objectInn.hasOwnProperty(findeKaysArray[n])) {
-            console.log(objectInn[findeKaysArray[n]]);
+const getIn = (objectInn, findKaysArray) => {
+    let objectInnKeys = Object.keys(objectInn);
+    objectInnKeys.forEach(function (item) {
+        if (typeof (item) == 'object') {
+            return getIn(item, findKaysArray);
         } else {
-            for (const key of Object.keys(objectInn)) {
-                recTestKeys(key);
+            return recTestKeys(item);
+        }
+    });
+
+
+    function recTestKeys(key) {
+        for (let i = 0; i < findKaysArray.length; i++) {
+            if (key.hasOwnProperty(findKaysArray[i])) {
+
             }
         }
     }
@@ -52,6 +47,33 @@ getIn(data, ['hosts', 1, 'name']); // 'web2'
   // getIn(data, ['hosts', 0]); // { name: 'web1' }
   // getIn(data, ['hosts', 1, null]); // 3
   // getIn(data, ['hosts', 1, 'active']); // false
+
+// const getIn = (objectInn, findeKaysArray) => {
+//     const recTestKeys = key => {
+//         for (let i = 0; i <= findeKaysArray.length; i++) {
+//             if (key.hasOwnProperty(findeKaysArray[i])) {
+//                 return objectInn[findeKaysArray[i]];
+//                 // console.log(objectInn[findeKaysArray[i]]);
+//             } else {
+//                 return null;
+//                 // console.log('no such key');
+//             }
+//         }
+//         // return objectInn[findeKaysArray[i]];
+//     }
+//     console.log(findeKaysArray.length);
+//     for (let n = 0; n < findeKaysArray.length; n++) {
+//         if (objectInn.hasOwnProperty(findeKaysArray[n])) {
+//             console.log(objectInn[findeKaysArray[n]]);
+//         } else {
+//             for (const key of Object.keys(objectInn)) {
+//                 recTestKeys(key);
+//             }
+//         }
+//     }
+// }
+
+
 
 
 // const data = {
