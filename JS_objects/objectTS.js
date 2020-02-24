7564cf1c7735bbe68332538455a883610df992fb
Vitaly <vitalykuklin@gmail.com>
2020-02-24 20:31:18 +0700

    Feature: work with hexlet.objects.task [ref: objects tasks]

--- JS_objects/objectTS.js
+++ JS_objects/objectTS.js
@@ -1,55 +1,94 @@
-"use strict";
-const data = {
-    user: 'ubuntu',
+//// new version (2)  new algorithm
+// whats write? =>
+// Object.entries() ?
+// obj.hasOwnProperty('propertyName') ?
+// lodash _.has(object, path)
+// lets first check obj.hasOwnProperty
+var data = {
+    user: "ubuntu",
     hosts: {
         0: {
-            name: 'web1',
+            name: "web1"
         },
         1: {
-            name: 'web2',
-            null: 3,
-            active: false,
-        },
-    },
-};
-const getIn = (objectInn, findKaysArray) => {
-    if (findKaysArray.length == 0 || findKaysArray[0] == null || findKaysArray[0] == 'undefined')
-        return null;
-    let objectInnKeys = Object.keys(objectInn);
-    for (let j = 0; j < objectInnKeys.length; j++) {
-        let findKey = recTestKeys(objectInnKeys[j]);
-        if (findKey !== null) {
-            if (typeof (findKey) == 'object') {
-                return getIn(findKey, findKaysArray);
-            }
-            else if (j == findKaysArray.length - 1) {
-                return findKey;
-            }
-            else {
-                return null;
-            }
-        } // TODO: return findKey; its right? need return?
-    }
-    function recTestKeys(key) {
-        for (let i = 0; i < findKaysArray.length; i++) {
-            // console.log(objectInn[key])
-            if (key == findKaysArray[i]) {
-                return objectInn[key];
-            }
+            name: "web2",
+            "null": 3,
+            active: false
         }
-        return null; // TODO: its right?
     }
 };
-console.log(getIn(data, ['undefined'])); //+
-// getIn(data, ['undefined']); // null
-console.log(getIn(data, ['user'])); // 'ubuntu' //+
-// getIn(data, ['user', 'ubuntu']); // null
-console.log(getIn(data, ['user', 'ubuntu'])); //+
-// getIn(data, ['hosts', 1, 'name']); // 'web2'
-console.log(getIn(data, ['hosts', 1, 'name']));
-// getIn(data, ['hosts', 0]); // { name: 'web1' }
-console.log(getIn(data, ['hosts', 0]));
-// getIn(data, ['hosts', 1, null]); // 3
-console.log(getIn(data, ['hosts', 1, null]));
-// getIn(data, ['hosts', 1, 'active']); // false //
-console.log(getIn(data, ['hosts', 1, 'active'])); // + неожиданное прошел этот тест
+var getIn = function (objectInn, findKaysArray) {
+    if (findKaysArray.length == 0 ||
+        findKaysArray[0] == null ||
+        findKaysArray[0] == "undefined")
+        return null;
+    var findKaysArrayToString = findKaysArray.toString().split(","); // any input types => string
+    var kaysArrLen = findKaysArrayToString.length;
+    function recTestKeys(obj, keys) {
+        var i = 0;
+        if (i == kaysArrLen)
+            return obj[keys["i"]];
+        if (obj.hasOwnProperty(keys["i"])) {
+            return recTestKeys(obj[keys["i"]], keys[i + 1]);
+        }
+    }
+    return recTestKeys(objectInn, findKaysArrayToString);
+};
+console.log(getIn(data, ["undefined"])); // null //+
+console.log(getIn(data, ["user"])); // 'ubuntu' //+
+console.log(getIn(data, ["user", "ubuntu"])); // null//+
+console.log(getIn(data, ["hosts", 1, "name"])); // 'web2'
+console.log(getIn(data, ["hosts", 0])); // { name: 'web1' }
+console.log(getIn(data, ["hosts", 1, null])); // 3
+console.log(getIn(data, ["hosts", 1, "active"])); // false
+/// pervious version  look at +/-
+// const data = {
+//   user: "ubuntu",
+//   hosts: {
+//     0: {
+//       name: "web1"
+//     },
+//     1: {
+//       name: "web2",
+//       null: 3,
+//       active: false
+//     }
+//   }
+// };
+// const getIn = (objectInn: object, findKaysArray: any[]): any => {
+//   if (
+//     findKaysArray.length == 0 ||
+//     findKaysArray[0] == null ||
+//     findKaysArray[0] == "undefined"
+//   )
+//     return null;
+//   let objectInnKeys: Array<any> = Object.keys(objectInn);
+//   for (let j = 0; j < objectInnKeys.length; j++) {
+//     let findKey: string = recTestKeys(objectInnKeys[j]);
+//     if (findKey !== null) {
+//       if (typeof findKey == "object") {
+//         return getIn(findKey, findKaysArray);
+//       } else if (j == findKaysArray.length - 1) {
+//         return findKey;
+//       } else {
+//         return [findKey]; //
+//       }
+//     } //
+//   }
+//   function recTestKeys(key: any) {
+//     for (let i = 0; i < findKaysArray.length; i++) {
+//       // console.log(objectInn[key])
+//       if (key == findKaysArray[i]) {
+//         return objectInn[key];
+//       }
+//     }
+//     return null; //
+//   }
+// };
+// console.log(getIn(data, ["undefined"])); // null //+
+// console.log(getIn(data, ["user"])); // 'ubuntu' //+
+// console.log(getIn(data, ["user", "ubuntu"])); // null//  not work with 'else {return [find kay} in last if...else'
+// console.log(getIn(data, ["hosts", 1, "name"])); // +  'web2'
+// console.log(getIn(data, ["hosts", 0])); // { name: 'web1' }
+// console.log(getIn(data, ["hosts", 1, null])); // 3
+// console.log(getIn(data, ["hosts", 1, "active"])); // false
--- JS_objects/objectTS.ts
+++ JS_objects/objectTS.ts
@@ -1,54 +1,104 @@
+//// new version (2)  new algorithm
+// whats write? =>
+// Object.entries() ?
+// obj.hasOwnProperty('propertyName') ?
+// lodash _.has(object, path)
+
+// lets first check obj.hasOwnProperty
+
 const data = {
-    user: 'ubuntu',
-    hosts: {
-        0: {
-            name: 'web1',
-        },
-        1: {
-            name: 'web2',
-            null: 3,
-            active: false,
-        },
+  user: "ubuntu",
+  hosts: {
+    0: {
+      name: "web1"
     },
+    1: {
+      name: "web2",
+      null: 3,
+      active: false
+    }
+  }
 };
 
-const getIn = (objectInn: object, findKaysArray: string[]): any => {
-    if (findKaysArray.length == 0 || findKaysArray[0] == null || findKaysArray[0] == 'undefined') return null;
-    let objectInnKeys: Array<any> = Object.keys(objectInn);
-    for (let j = 0; j < objectInnKeys.length; j++) {
-        let findKey: string = recTestKeys(objectInnKeys[j]);
-        if (findKey !== null) {
-            if (typeof (findKey) == 'object') {
-                return getIn(findKey, findKaysArray);
-            } else if (j == findKaysArray.length - 1) {
-                return findKey;
-            } else {
-                return null;
-            }
-        }    // TODO: return findKey; its right? need return?
+const getIn = (objectInn: object, findKaysArray: any[]): any => {
+  if (
+    findKaysArray.length == 0 ||
+    findKaysArray[0] == null ||
+    findKaysArray[0] == "undefined"
+  )
+    return null;
+  const findKaysArrayToString: string[] = findKaysArray.toString().split(","); // any input types => string
+  const kaysArrLen = findKaysArrayToString.length;
+  function recTestKeys(obj: object, keys: any[]) {
+    let i: number = 0;
+    if (i == kaysArrLen) return obj[keys["i"]];
+    if (obj.hasOwnProperty(keys["i"])) {
+      return recTestKeys(obj[keys["i"]], keys[i + 1]);
     }
+  }
+  return recTestKeys(objectInn, findKaysArrayToString);
+};
 
-    function recTestKeys(key) {
-        for (let i = 0; i < findKaysArray.length; i++) {
-            // console.log(objectInn[key])
-            if (key == findKaysArray[i]) {
-                return objectInn[key];
-            }
-        }
-        return null; // TODO: its right?
-    }
-}
+console.log(getIn(data, ["undefined"])); // null //+
+console.log(getIn(data, ["user"])); // 'ubuntu' //+
+console.log(getIn(data, ["user", "ubuntu"])); // null//+
+console.log(getIn(data, ["hosts", 1, "name"])); // 'web2'
+console.log(getIn(data, ["hosts", 0])); // { name: 'web1' }
+console.log(getIn(data, ["hosts", 1, null])); // 3
+console.log(getIn(data, ["hosts", 1, "active"])); // false
 
-console.log(getIn(data, ['undefined'])); //+
-// getIn(data, ['undefined']); // null
-console.log(getIn(data, ['user'])); // 'ubuntu' //+
-// getIn(data, ['user', 'ubuntu']); // null
-console.log(getIn(data, ['user', 'ubuntu'])); //+
-// getIn(data, ['hosts', 1, 'name']); // 'web2'
-console.log(getIn(data, ['hosts', 1, 'name']));
-// getIn(data, ['hosts', 0]); // { name: 'web1' }
-console.log(getIn(data, ['hosts', 0]));
-// getIn(data, ['hosts', 1, null]); // 3
-console.log(getIn(data, ['hosts', 1, null]));
-// getIn(data, ['hosts', 1, 'active']); // false
-console.log(getIn(data, ['hosts', 1, 'active']));
+/// pervious version  look at +/-
+
+// const data = {
+//   user: "ubuntu",
+//   hosts: {
+//     0: {
+//       name: "web1"
+//     },
+//     1: {
+//       name: "web2",
+//       null: 3,
+//       active: false
+//     }
+//   }
+// };
+
+// const getIn = (objectInn: object, findKaysArray: any[]): any => {
+//   if (
+//     findKaysArray.length == 0 ||
+//     findKaysArray[0] == null ||
+//     findKaysArray[0] == "undefined"
+//   )
+//     return null;
+//   let objectInnKeys: Array<any> = Object.keys(objectInn);
+//   for (let j = 0; j < objectInnKeys.length; j++) {
+//     let findKey: string = recTestKeys(objectInnKeys[j]);
+//     if (findKey !== null) {
+//       if (typeof findKey == "object") {
+//         return getIn(findKey, findKaysArray);
+//       } else if (j == findKaysArray.length - 1) {
+//         return findKey;
+//       } else {
+//         return [findKey]; //
+//       }
+//     } //
+//   }
+
+//   function recTestKeys(key: any) {
+//     for (let i = 0; i < findKaysArray.length; i++) {
+//       // console.log(objectInn[key])
+//       if (key == findKaysArray[i]) {
+//         return objectInn[key];
+//       }
+//     }
+//     return null; //
+//   }
+// };
+
+// console.log(getIn(data, ["undefined"])); // null //+
+// console.log(getIn(data, ["user"])); // 'ubuntu' //+
+// console.log(getIn(data, ["user", "ubuntu"])); // null//  not work with 'else {return [find kay} in last if...else'
+// console.log(getIn(data, ["hosts", 1, "name"])); // +  'web2'
+// console.log(getIn(data, ["hosts", 0])); // { name: 'web1' }
+// console.log(getIn(data, ["hosts", 1, null])); // 3
+// console.log(getIn(data, ["hosts", 1, "active"])); // false
