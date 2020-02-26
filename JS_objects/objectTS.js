6f097dfcd631f259c9eb6cb26fa8e807c99986ec
kuklinv <vitalykuklin@gmail.com>
2020-02-26 10:41:27 +0700

    Fix: sinc local repo

--- JS_objects/objectTS.js
+++ JS_objects/objectTS.js
@@ -1,55 +1,57 @@
 "use strict";
 const data = {
-    user: 'ubuntu',
-    hosts: {
-        0: {
-            name: 'web1',
-        },
-        1: {
-            name: 'web2',
-            null: 3,
-            active: false,
-        },
+  user: "ubuntu",
+  hosts: {
+    0: {
+      name: "web1"
     },
+    1: {
+      name: "web2",
+      null: 3,
+      active: false
+    }
+  }
 };
 const getIn = (objectInn, findKaysArray) => {
-    if (findKaysArray.length == 0 || findKaysArray[0] == null || findKaysArray[0] == 'undefined')
+  if (
+    findKaysArray.length == 0 ||
+    findKaysArray[0] == null ||
+    findKaysArray[0] == "undefined"
+  )
+    return null;
+  let objectInnKeys = Object.keys(objectInn);
+  for (let j = 0; j < objectInnKeys.length; j++) {
+    let findKey = recTestKeys(objectInnKeys[j]);
+    if (findKey !== null) {
+      if (typeof findKey == "object") {
+        return getIn(findKey, findKaysArray);
+      } else if (j == findKaysArray.length - 1) {
+        return findKey;
+      } else {
         return null;
-    let objectInnKeys = Object.keys(objectInn);
-    for (let j = 0; j < objectInnKeys.length; j++) {
-        let findKey = recTestKeys(objectInnKeys[j]);
-        if (findKey !== null) {
-            if (typeof (findKey) == 'object') {
-                return getIn(findKey, findKaysArray);
-            }
-            else if (j == findKaysArray.length - 1) {
-                return findKey;
-            }
-            else {
-                return null;
-            }
-        } // TODO: return findKey; its right? need return?
+      }
+    } // TODO: return findKey; its right? need return?
+  }
+  function recTestKeys(key) {
+    for (let i = 0; i < findKaysArray.length; i++) {
+      // console.log(objectInn[key])
+      if (key == findKaysArray[i]) {
+        return objectInn[key];
+      }
     }
-    function recTestKeys(key) {
-        for (let i = 0; i < findKaysArray.length; i++) {
-            // console.log(objectInn[key])
-            if (key == findKaysArray[i]) {
-                return objectInn[key];
-            }
-        }
-        return null; // TODO: its right?
-    }
+    return null; // TODO: its right?
+  }
 };
-console.log(getIn(data, ['undefined'])); //+
+console.log(getIn(data, ["undefined"])); //+
 // getIn(data, ['undefined']); // null
-console.log(getIn(data, ['user'])); // 'ubuntu' //+
+console.log(getIn(data, ["user"])); // 'ubuntu' //+
 // getIn(data, ['user', 'ubuntu']); // null
-console.log(getIn(data, ['user', 'ubuntu'])); //+
+console.log(getIn(data, ["user", "ubuntu"])); //+
 // getIn(data, ['hosts', 1, 'name']); // 'web2'
-console.log(getIn(data, ['hosts', 1, 'name']));
+console.log(getIn(data, ["hosts", 1, "name"]));
 // getIn(data, ['hosts', 0]); // { name: 'web1' }
-console.log(getIn(data, ['hosts', 0]));
+console.log(getIn(data, ["hosts", 0]));
 // getIn(data, ['hosts', 1, null]); // 3
-console.log(getIn(data, ['hosts', 1, null]));
+console.log(getIn(data, ["hosts", 1, null]));
 // getIn(data, ['hosts', 1, 'active']); // false //
-console.log(getIn(data, ['hosts', 1, 'active'])); // + неожиданное прошел этот тест
+console.log(getIn(data, ["hosts", 1, "active"])); // + неожиданное прошел этот тест
