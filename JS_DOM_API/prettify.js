31544be0005da736b06e62c5d2b0e0234100ddfb
Vitaly Kuklin <vitalykuklin@gmail.com>
2020-05-21 15:36:53 +0700

    Feature: add new var [not working]

--- JS_DOM_API/prettify.js
+++ JS_DOM_API/prettify.js
@@ -1,25 +1,42 @@
 // my work
 let doc = document.body;
 function prettify(doc) {
-    let targetDiv = document.querySelectorAll('div');
-    for (let i = 1; i < targetDiv.length; i += 1) {
-        if (!targetDiv[i].textContent) {
-            continue;
-        }
-        let p = document.createElement('p');
-        let text = targetDiv[i].textContent.trim();
-        p.innerHTML = text;
-        targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
-        // targetDiv[i].childNodes[0].replaceWith(p);
-        // doc.appendChild()
+  let targetDiv = document.querySelectorAll("div");
+  for (let divEl of targetDiv) {
+    if (divEl.textContent) {
+      let p = document.createElement("p");
+      let text = divEl.textContent.trim();
+      p.innerHTML = text;
+      //   divEl.replaceWith(p);
+      divEl.replaceChild(p, divEl.childNodes[0]);
     }
-    return document.body.innerHTML;
+
+    // targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
+    // targetDiv[i].childNodes[0].replaceWith(p);
+    // doc.appendChild()
+  }
+  return document.body.innerHTML;
 }
-const elements = prettify(doc);
-// doc.innerHTML = elements;
-console.log(document.body.innerHTML);
 
-
+// let doc = document.body;
+// function prettify(doc) {
+//     let targetDiv = document.querySelectorAll('div');
+//     for (let i = 1; i < targetDiv.length; i += 1) {
+//         if (!targetDiv[i].textContent) {
+//             continue;
+//         }
+//         let p = document.createElement('p');
+//         let text = targetDiv[i].textContent.trim();
+//         p.innerHTML = text;
+//         targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
+//         // targetDiv[i].childNodes[0].replaceWith(p);
+//         // doc.appendChild()
+//     }
+//     return document.body.innerHTML;
+// }
+// const elements = prettify(doc);
+// // doc.innerHTML = elements;
+// console.log(document.body.innerHTML);
 
 // hexlet
 // решение не проходит тесты с вложенными div друг в друга
