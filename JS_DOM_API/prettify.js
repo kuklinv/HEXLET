aef34c16b25cc56f5e0a908a9359d1b494780a0e
Vitaly Kuklin <vitalykuklin@gmail.com>
2020-05-22 12:04:25 +0700

    Feature: prettify passed 1,3,4 filed 2,5,6

--- JS_DOM_API/prettify.js
+++ JS_DOM_API/prettify.js
@@ -1,24 +1,42 @@
-// my work
-function prettify(document) {
-  let targetDiv = document.querySelectorAll("div");
-  for (let divEl of targetDiv) {
-    if (divEl.textContent) {
-      let p = document.createElement("p");
-      let text = divEl.textContent.trim();
-      p.innerHTML = text;
-      // divEl.remove(divEl.children);
-      divEl.replaceWith(p); //TODO: not remove pre node
-      document.body.append(divEl);
-    }
+// passed 1,3,4
+// filed 2,5,6
 
-    // targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
+function prettify(doc) {
+  let targetDiv = document.querySelectorAll("div");
+  for (let i = 1; i < targetDiv.length; i += 1) {
+    if (!targetDiv[i].textContent) {
+      continue;
+    }
+    let p = document.createElement("p");
+    let text = targetDiv[i].textContent.trim();
+    p.innerHTML = text;
+    targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
     // targetDiv[i].childNodes[0].replaceWith(p);
     // doc.appendChild()
   }
   return document.body.innerHTML;
-}
+} // my work
 
-export default prettify;
+// function prettify(document) {
+//   let targetDiv = document.querySelectorAll("div");
+//   for (let divEl of targetDiv) {
+//     if (divEl.textContent) {
+//       let p = document.createElement("p");
+//       let text = divEl.textContent.trim();
+//       p.innerHTML = text;
+//       // divEl.remove(divEl.children);
+//       divEl.replaceWith(p); //TODO: not remove pre node
+//       document.body.append(divEl);
+//     }
+
+//     // targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
+//     // targetDiv[i].childNodes[0].replaceWith(p);
+//     // doc.appendChild()
+//   }
+//   return document.body.innerHTML;
+// }
+
+// export default prettify;
 
 // function prettify(document) {
 //   let targetDiv = document.querySelectorAll("div");
@@ -40,22 +58,8 @@ export default prettify;
 //   return document.body.innerHTML;
 // }
 
-// let doc = document.body;
-// function prettify(doc) {
-//     let targetDiv = document.querySelectorAll('div');
-//     for (let i = 1; i < targetDiv.length; i += 1) {
-//         if (!targetDiv[i].textContent) {
-//             continue;
-//         }
-//         let p = document.createElement('p');
-//         let text = targetDiv[i].textContent.trim();
-//         p.innerHTML = text;
-//         targetDiv[i].replaceChild(p, targetDiv[i].childNodes[0]);
-//         // targetDiv[i].childNodes[0].replaceWith(p);
-//         // doc.appendChild()
-//     }
-//     return document.body.innerHTML;
-// }
+// let doc = document.body;                                                /// working var but not all tests
+
 // const elements = prettify(doc);
 // // doc.innerHTML = elements;
 // console.log(document.body.innerHTML);
