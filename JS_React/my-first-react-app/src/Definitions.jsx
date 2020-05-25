118e48b976a3b9466b98ade5475cd9021995e62d
Vitaly <vitalykuklin@gmail.com>
2020-05-25 21:55:25 +0700

    Feature: add mod definitions component [ref:no-ref]

--- JS_React/my-first-react-app/src/Definitions.jsx
+++ JS_React/my-first-react-app/src/Definitions.jsx
@@ -1,22 +1,20 @@
+// import { uniqueId } from 'lodash';
 import React from "react";
 
-export default class Definitions {
-    render(){
-        const {data} = this.props;
-
-        return(
-            <dl>
-                {data.map(function ({dt, dd}) {
-                    return(
-                    <React.Fragment>
-                    <dt key={dt.id}>{dt}</dt>
-                    <dd key={dd.id}>{dd}</dd>
-                    </React.Fragment>)
-                })
-                }
-            </dl>
-        );
-    }
+export default class Definitions extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <dl>
+        {data.map(({ dt, dd }) => (
+          <React.Fragment key={{ dt, dd }.id}>
+            <dt>{dt}</dt>
+            <dd>{dd}</dd>
+          </React.Fragment>
+        ))}
+      </dl>
+    );
+  }
 }
 
 // <React.Fragment>
