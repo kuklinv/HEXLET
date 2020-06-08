666056104fffefddefae0e53dc57f13e3c051501
kuklinv <vitalykuklin@gmail.com>
2020-06-08 20:11:47 +0700

    Feature: work with JS_function 8[ref:no-ref]

--- JS_functions/functions.js
+++ JS_functions/functions.js
@@ -1,8 +1,21 @@
+const users = [
+    { name: 'Tirion', birthday: 'Nov 19, 1988' },
+    { name: 'Sam', birthday: 'Nov 22, 1999' },
+    { name: 'Rob', birthday: 'Jan 11, 1975' },
+    { name: 'Sansa', birthday: 'Mar 20, 2001' },
+    { name: 'Tisha', birthday: 'Feb 27, 1992' },
+    { name: 'Chris', birthday: 'Dec 25, 1995' },
+];
 
+const takeOldest = (usrersArray, userNum = 1) => {
 
+}
 
-
-
+let result = takeLast(users);
+console.log(result);
+// [
+//   { name: 'Rob', birthday: 'Jan 11, 1975' },
+// ];
 
 // const takeLast = (string, numberChar) => {
 //     if (string.length === 0 || numberChar > string.length) return null;
