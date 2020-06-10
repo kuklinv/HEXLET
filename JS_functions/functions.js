33f89ca31f8ae01ad0b5c8d2ba030137d24f67be
Vitaly <vitalykuklin@gmail.com>
2020-06-10 22:37:00 +0700

    Feature: task abour reduce [ref:#8 hash]

--- JS_functions/functions.js
+++ JS_functions/functions.js
@@ -1,18 +1,50 @@
+
 const users = [
-    { name: 'Tirion', birthday: 'Nov 19, 1988' },
-    { name: 'Sam', birthday: 'Nov 22, 1999' },
-    { name: 'Rob', birthday: 'Jan 11, 1975' },
-    { name: 'Sansa', birthday: 'Mar 20, 2001' },
-    { name: 'Tisha', birthday: 'Feb 27, 1992' },
-    { name: 'Chris', birthday: 'Dec 25, 1995' },
+    { name: 'Bronn', gender: 'male', birthday: '1973-03-23' },
+    { name: 'Reigar', gender: 'male', birthday: '1973-11-03' },
+    { name: 'Eiegon', gender: 'male', birthday: '1963-11-03' },
+    { name: 'Sansa', gender: 'female', birthday: '2012-11-03' },
+    { name: 'Jon', gender: 'male', birthday: '1980-11-03' },
+    { name: 'Robb', gender: 'male', birthday: '1980-05-14' },
+    { name: 'Tisha', gender: 'female', birthday: '2012-11-03' },
+    { name: 'Rick', gender: 'male', birthday: '2012-11-03' },
+    { name: 'Joffrey', gender: 'male', birthday: '1999-11-03' },
+    { name: 'Edd', gender: 'male', birthday: '1973-11-03' },
 ];
 
-const takeOldest = (usrersArray, userNum = 1) => {
+const getMenCountByYear = (data) => {
 
 }
 
-let result = takeLast(users);
-console.log(result);
+/*
+Реализуйте и экспортируйте по умолчанию функцию getMenCountByYear, которая принимает на вход список
+ пользователей и возвращает объект,
+в котором ключ это год рождения, а значение это количество мужчин, родившихся в этот год.
+ */
+getMenCountByYear(users);
+// {
+//   1973: 3,
+//   1963: 1,
+//   1980: 2,
+//   2012: 1,
+//   1999: 1,
+// };
+
+// const users = [
+//     { name: 'Tirion', birthday: 'Nov 19, 1988' },
+//     { name: 'Sam', birthday: 'Nov 22, 1999' },
+//     { name: 'Rob', birthday: 'Jan 11, 1975' },
+//     { name: 'Sansa', birthday: 'Mar 20, 2001' },
+//     { name: 'Tisha', birthday: 'Feb 27, 1992' },
+//     { name: 'Chris', birthday: 'Dec 25, 1995' },
+// ];
+//
+// const takeOldest = (usrersArray, userNum = 1) => {
+//
+// }
+//
+// let result = takeLast(users);
+// console.log(result);
 // [
 //   { name: 'Rob', birthday: 'Jan 11, 1975' },
 // ];
