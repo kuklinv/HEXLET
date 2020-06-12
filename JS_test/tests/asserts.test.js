e5d4109737ccc62c8736798623ded6ae624c7b6b
Vitaly <vitalykuklin@gmail.com>
2020-06-12 14:30:10 +0700

    Feature: asserts task [ref:no-ref]

--- JS_test/tests/asserts.test.js
+++ JS_test/tests/asserts.test.js
@@ -6,4 +6,5 @@
 assert.deepStrictEqual(take([1,2,3],2),[1,2]);
 assert.deepStrictEqual(take([],2),[]);
 assert.deepStrictEqual(take([4,3],9), [4,3]);
-assert.deepStrictEqual(take([1,2,3,4,5,6],5),[1,2,3,4,5]); //???
+assert.deepStrictEqual(take([1,2,3]),[1]); //???
+assert.deepStrictEqual(take([1,2,3],0),[]);
