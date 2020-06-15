cb79a882316d5b556c5a8d00d172b359e4c9c1ef
Vitaly <vitalykuklin@gmail.com>
2020-06-15 21:03:06 +0700

    Feature: add jest new  ex 6-jest [ref:no-ref]

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,18 @@
 import {reverse} from '../JS_test';
 import {expect} from "@jest/globals";
+//
+// test('reverse', () => {
+//     expect(reverse('hello')).toEqual('olleh');
+//     expect(reverse('')).toEqual('');
+// });
 
-test('reverse', () => {
-    expect(reverse('hello')).toEqual('olleh');
-    expect(reverse('')).toEqual('');
-});
+// @ts-check
+
+import getFunction from '../JS_test/tests/functionJest.js';
+
+const without = getFunction();
+
+// BEGIN (write your solution here)
+
+// END
+
