13739a9e9519b4b6027ee81b59aab31662477822
Vitaly <vitalykuklin@gmail.com>
2020-06-16 23:16:56 +0700

    Feature: add validator ex for JS_test and chunk for trials [ref:no-ref]

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,38 +1,38 @@
-// import {reverse} from '../JS_test';
-// import {expect} from "@jest/globals";
+// // import {reverse} from '../JS_test';
+// // import {expect} from "@jest/globals";
+// //
+// // test('reverse', () => {
+// //     expect(reverse('hello')).toEqual('olleh');
+// //     expect(reverse('')).toEqual('');
+// // });
 //
-// test('reverse', () => {
-//     expect(reverse('hello')).toEqual('olleh');
-//     expect(reverse('')).toEqual('');
+// // @ts-check
+//
+// import getFunction from '../JS_test/tests/functionJest.js';
+//
+// const const gt = getFunction();
+//
+// test('gt', () => {
+//     expect(gt(3, 1)).toBeTruthy();
+//     expect(gt(3, 3)).toBeFalsy();
+//     expect(gt(1, 3)).toBeFalsy();
 // });
-
-// @ts-check
-
-import getFunction from '../JS_test/tests/functionJest.js';
-
-const const gt = getFunction();
-
-test('gt', () => {
-    expect(gt(3, 1)).toBeTruthy();
-    expect(gt(3, 3)).toBeFalsy();
-    expect(gt(1, 3)).toBeFalsy();
-});
-
-// _.gt(3, 1);
-// // => true
 //
-// _.gt(3, 3);
-// // => false
+// // _.gt(3, 1);
+// // // => true
+// //
+// // _.gt(3, 3);
+// // // => false
+// //
+// // _.gt(1, 3);
+// // // => false
 //
-// _.gt(1, 3);
-// // => false
-
+// //
+// // // BEGIN (write your solution here)
+// // test('without', () => {
+// //     expect(without([2, 1, 2, 3], 1, 2)).toEqual([3]);
+// //     expect(without([2, 1, 2, 3], 4, 5)).toEqual([2,1,2,3]);
+// //     expect(without([],4,5)).toEqual([]);
+// // });
+// // // END
 //
-// // BEGIN (write your solution here)
-// test('without', () => {
-//     expect(without([2, 1, 2, 3], 1, 2)).toEqual([3]);
-//     expect(without([2, 1, 2, 3], 4, 5)).toEqual([2,1,2,3]);
-//     expect(without([],4,5)).toEqual([]);
-// });
-// // END
-
---
+++ __tests__/validator.test.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+import _ from 'lodash';
+import getImplementation from '../implementations/index.js';
+
+const makeValidator = getImplementation();
+
+// BEGIN (write your solution here)
+
+// END
+
+// const validator = makeValidator();
+// validator.isValid('some value'); // true
+// validator.addCheck((v) => v > 5);
+// validator.isValid(3); // false
+// validator.isValid(8); // true
+// validator.addCheck(/* add more checks */);
