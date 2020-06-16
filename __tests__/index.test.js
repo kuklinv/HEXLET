36c61a131cfeb56dd31f8750f86a50ca31f961b0
Vitaly <vitalykuklin@gmail.com>
2020-06-16 07:42:39 +0700

    Feature: 6 ex jest - solved [ref:no-ref]

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
-import {reverse} from '../JS_test';
-import {expect} from "@jest/globals";
+// import {reverse} from '../JS_test';
+// import {expect} from "@jest/globals";
 //
 // test('reverse', () => {
 //     expect(reverse('hello')).toEqual('olleh');
@@ -13,6 +13,10 @@ import getFunction from '../JS_test/tests/functionJest.js';
 const without = getFunction();
 
 // BEGIN (write your solution here)
-
+test('without', () => {
+    expect(without([2, 1, 2, 3], 1, 2)).toEqual([3]);
+    expect(without([2, 1, 2, 3], 4, 5)).toEqual([2,1,2,3]);
+    expect(without([],4,5)).toEqual([]);
+});
 // END
 
