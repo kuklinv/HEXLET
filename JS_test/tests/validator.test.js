3488fe54e3429ded32f36f623044e3f04de2eb3a
kuklinv <vitalykuklin@gmail.com>
2020-06-19 22:49:55 +0700

    Feature: add validator test[ref:no-ref]

---
+++ JS_test/tests/validator.test.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+import _ from 'lodash';
+import getImplementation from '../implementations/index.js';
+
+const makeValidator = getImplementation();
+
+// BEGIN (write your solution here)
+
+// END
