4caff1f659f4929fd1520641c96c89e13a8d43eb
Vitaly <vitalykuklin@gmail.com>
2020-08-02 10:30:14 +0700

    Feature: solved module-test with jest [ref:no-ref]

--- JS_test/hexlet-jest/_tests_/validator.test.js
+++ JS_test/hexlet-jest/_tests_/validator.test.js
@@ -1,8 +1,13 @@
-import _ from "lodash";
 import getImplementation from "../implementations/index.js";
 
 const makeValidator = getImplementation();
 
 // BEGIN (write your solution here)
-
+const validator = makeValidator();
+test("simple", () => {
+    expect(validator.isValid('some value')).toBe(true);
+    validator.addCheck((v) => v > 5);
+    expect(validator.isValid(3)).toBe(false);
+    expect(validator.isValid(8)).toBe(true);
+});
 // END
