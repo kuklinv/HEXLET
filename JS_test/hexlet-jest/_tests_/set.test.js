eeb11fabc402ad687acaebd82a51989829f1463f
Vitaly <vitalykuklin@gmail.com>
2020-08-03 22:16:31 +0700

    Feature: continue with set.tests [ref:no-ref]

--- JS_test/hexlet-jest/_tests_/set.test.js
+++ JS_test/hexlet-jest/_tests_/set.test.js
@@ -10,14 +10,35 @@ const set = getFunction();
 test('set1', () => {
     const object = {};
     set(object, 'a[0].b.c', 4);
-    const res = object[0][][];
-    expect(res).toBe(4);
+    //TODO: need realise object's value
+    let {
+        a: [
+            {
+                b: {
+                    c: value
+                }
+            }
+        ]
+    } = object;
+    // const res = a[0].b.c;
+    expect(value).toBe(4);
 })
 
 test('set2', () => {
     const object = {};
     set(object, ['x', '0', 'y', 'z'], 5);
-    const res = object[0][][];
-    expect(res).toBe(5);
+    //TODO: need realise object's value
+    let {
+        a: [
+            {
+                b: {
+                    c: value
+                }
+            }
+        ]
+    } = object;
+    const res = a[0].b.c;
+    // const res = object.[0][][];
+    expect(value).toBe(5);
 });
 
