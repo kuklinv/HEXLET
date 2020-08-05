9f57cd4322f36394dd974b95a7fd97a96fc98c5f
Vitaly <vitalykuklin@gmail.com>
2020-08-05 20:51:25 +0700

    Feature: continue with set.tests not realise beforeEach  - dont understend how clone object [ref:no-ref]

--- JS_test/hexlet-jest/_tests_/set.test.js
+++ JS_test/hexlet-jest/_tests_/set.test.js
@@ -1,15 +1,17 @@
 import getFunction from '../src/functions.js';
-import {test} from "@jest/globals";
+import {beforeEach, test} from "@jest/globals";
 
 const set = getFunction();
 
 // beforeEach(() => {
-//     const object = {};
-// })
+//     // const object = {};
+// const copy = cloneDeep(object);
+// });
 
 test('set1', () => {
     const object = {};
     set(object, 'a[0].b.c', 4);
+    // console.log(object);
     //TODO: need realise object's value
     let {
         a: [
@@ -27,6 +29,7 @@ test('set1', () => {
 test('set2', () => {
     const object = {};
     set(object, ['x', '0', 'y', 'z'], 5);
+    // console.log(object);
     //TODO: need realise object's value
     let {
         a: [
@@ -37,6 +40,11 @@ test('set2', () => {
             }
         ]
     } = object;
+    // console.log(a[0]);
+    // console.log(b);
+    // console.log(c);
+    // console.log([c]);
+
     const res = a[0].b.c;
     // const res = object.[0][][];
     expect(value).toBe(5);
