84ce002cfd0df1daa071434cbcfb63cb1f090176
Vitaly <vitalykuklin@gmail.com>
2020-08-23 09:26:39 +0700

    Feature: set1 passed set2 passed  Expected tests to fail, but they passed.[ref:#12]

--- JS_test/hexlet-jest/_tests_/set.test.js
+++ JS_test/hexlet-jest/_tests_/set.test.js
@@ -17,7 +17,6 @@ let {
             }
         ]
     } = copy;
-    // const res = a[0].b.c;
     expect(value).toBe(4);
 })
 
@@ -26,17 +25,7 @@ test('set2', () => {
     const object = {};
     const copy = _.cloneDeep(object);
     set(copy, ['x', '0', 'y', 'z'], 5);
-    let {
-        a: [
-            {
-                b: {
-                    c: value
-                }
-            }
-        ]
-    } = copy;
-    // const res = a[0].b.c;
-    // const res = object.[0][][];
+    let value = copy.x[0].y.z
     expect(value).toBe(5);
 });
 
