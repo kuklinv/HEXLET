bba7602ca73feb6a99580af6b6cf8de5539eb731
Vitaly <vitalykuklin@gmail.com>
2020-08-30 14:43:33 +0700

    Feature: i continue needed  wrong test [ref:#12]

--- JS_test/hexlet-jest/_tests_/set.test.js
+++ JS_test/hexlet-jest/_tests_/set.test.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 
 const set = getFunction();
 
+const nativeObject = {};
 const object = {};
 
 beforeEach(()=> {
@@ -34,6 +35,12 @@ test('set2', () => {
     let value = object.x[0].y.z
     expect(value).toBe(5);
 });
+
+test('mybeWrong',()=>{
+    set(object, ['x', '0', 'y', 'z'], 5);
+    nativeObject.key = 5;
+    expect(object).toEqual(nativeObject);
+})
 
 // test('wrongSet1', ()=>{
 //     set(object,'a[0].b.c', 4);
