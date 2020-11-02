3b826967ddb0668164f86ad2190d1ef4d60d3c97
Vitaly <vitalykuklin@gmail.com>
2020-11-02 21:51:02 +0700

    Feature: just rename param[ref:#12]

--- JS_test/hexlet-jest/_tests_/set.test.js
+++ JS_test/hexlet-jest/_tests_/set.test.js
@@ -1,46 +1,43 @@
-import getFunction from '../src/functions.js';
-import {beforeEach, test} from "@jest/globals";
-import _ from 'lodash';
+import getFunction from "../src/functions.js";
+import { beforeEach, test } from "@jest/globals";
+import _ from "lodash";
 
 const set = getFunction();
 
 const nativeObject = {};
 const object = {};
 
-beforeEach(()=> {
-return  _.cloneDeep(object);
+beforeEach(() => {
+  return _.cloneDeep(object);
 });
 
-test('set1', () => {
-    // const object = {};
-    // const copy = _.cloneDeep(object);
-    set(object, 'a[0].b.c', 4);
-let {
-        a: [
-            {
-                b: {
-                    c: value
-                }
-            }
-        ]
-    } = object;
-    expect(value).toBe(4);
-})
-
-
-test('set2', () => {
-    // const object = {};
-    // const copy = _.cloneDeep(object);
-    set(object, ['x', '0', 'y', 'z'], 5);
-    let value = object.x[0].y.z
-    expect(value).toBe(5);
+test("set1", () => {
+  // const object = {};
+  // const copy = _.cloneDeep(object);
+  set(object, "a[0].b.c", 4);
+  let {
+    a: [
+      {
+        b: { c: value },
+      },
+    ],
+  } = object;
+  expect(value).toBe(4);
 });
 
-test('mybeWrong',()=>{
-    set(object, ['x', '0', 'y', 'z'], 5);
-    nativeObject.key = 5;
-    expect(object[0]).toEqual(nativeObject[0]);
-})
+test("set2", () => {
+  // const object = {};
+  // const copy = _.cloneDeep(object);
+  set(object, ["x", "0", "y", "z"], 5);
+  let value = object.x[0].y.z;
+  expect(value).toBe(5);
+});
+
+test("maybeWrong", () => {
+  set(object, ["x", "0", "y", "z"], 5);
+  nativeObject.key = 5;
+  expect(object[0]).toEqual(nativeObject[0]);
+});
 
 /*
 my be
@@ -62,4 +59,3 @@ console.log(deep[0] === objects[0]);
 //     object[a[0].b.c] = '4'
 //     expect(object.key).toBe('4');
 // });
-
