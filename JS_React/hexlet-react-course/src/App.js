270f76af6ac073ebd45521d4ac1149dd2c7cc0f3
Vitaly <vitalykuklin@gmail.com>
2020-12-23 06:02:17 +0700

    Feature: add buttomGroup component

--- JS_React/hexlet-react-course/src/App.js
+++ JS_React/hexlet-react-course/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from "./components/ButtonGroup/ButtonGroup.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Its React course on HEXLET
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                Its React course on HEXLET
+            </header>
+            <div>
+                <ButtonGroup/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- JS_React/hexlet-react-course/src/components/ButtonGroup/ButtonGroup.js
+++ JS_React/hexlet-react-course/src/components/ButtonGroup/ButtonGroup.js
@@ -1,2 +1,16 @@
 import React, {Component} from 'react'
+import {render} from "@testing-library/react";
 
+class ButtonGroup extends Component {
+    constructor(props) {
+        super(props);
+        render()
+        {
+            return (
+
+            )
+        }
+    }
+}
+
+export default ButtonGroup;
