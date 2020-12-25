9d9765a64ea5af895dd5f97561b25501cc0bbbff
Vitaly <vitalykuklin@gmail.com>
2020-12-25 07:05:22 +0700

    Feature: update repo with depend-s and try to make logic - not success[ref:no-ref]

--- JS_React/hexlet-react-course/src/components/ButtonGroup/ButtonGroup.js
+++ JS_React/hexlet-react-course/src/components/ButtonGroup/ButtonGroup.js
@@ -1,29 +1,42 @@
 import React, {Component} from 'react'
-import classes from './ButtonGroup.module.css';
+// import classes from './ButtonGroup.module.css';
+import cn from 'classnames';
 
 class ButtonGroup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            btnLeftActive: "btn btn-secondary left active",
-            btnLeftUnActive : "btn btn-secondary left",
-            btnRightActive: "btn btn-secondary right active",
-            btnRightUnActive: "btn btn-secondary right"
+            isPressedLeft: false,
+            isPressedRight: false,
+            btnSide: null
         }
     }
 
     btnLeftHandler = () => {
-
+        const pressedLeft = this.state.isPressedLeft;
+        // const pressedRight = this.state.isPressedRight;
+        pressedLeft ? this.setState({isPressedLeft: true, isPressedRight: false, btnSide: 'left'}) : this.setState({
+            isPressedLeft: false,
+            isPressedRight: true,
+            btnSide: 'right'
+        });
     }
-    btnRightHandler = () => {
+    // btnRightHandler = () => {
+    //
+    //
+    // }
 
-
-    }
     render() {
+        const {isPressedLeft, isPressedRight, btnSide} = this.state;
+        const btnClass = cn('btn', 'btn-secondary', {
+            'active': isPressedLeft,
+            'left': isPressedLeft,
+            'right': isPressedRight
+        });
         return (
             <div className="btn-group" role="group">
-                <button onClick={this.btnLeftHandler} type="button" className={this.state.btnLeftUnActive}>Left</button>
-                <button onClick={this.btnRightHandler} type="button" className={this.state.btnRightUnActive}>Right</button>
+                <button onClick={this.btnLeftHandler} type="button" className={btnClass}>Left</button>
+                <button onClick={this.btnRightHandler} type="button" className={btnClass}>Right</button>
             </div>
         )
     }
