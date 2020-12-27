44677229fb05a78ddb61b242874309932bd0b910
Vitaly <vitalykuklin@gmail.com>
2020-12-27 13:37:09 +0700

    Feature: refactor handler and state but not working - add topic on Hexlet, weiting response [ref:no-ref]

--- JS_React/hexlet-react-course/src/components/ButtonGroup/ButtonGroup.js
+++ JS_React/hexlet-react-course/src/components/ButtonGroup/ButtonGroup.js
@@ -1,49 +1,81 @@
 import React, {Component} from 'react'
-// import classes from './ButtonGroup.module.css';
-import cn from 'classnames';
+import cn from 'classnames'
 
-class ButtonGroup extends Component {
+// import classes from './ButtonGroup.module.css';
+
+class BtnGroup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            active: false,
-            btnSide: null
+            activeLeft: false,
+            activeRight: false
         }
     }
 
-    btnLeftHandler = (e) => () => {
+    btnLeftHandler = (e) => {
         const target = e.target;
         const btnSide = target.innerText;
         if (btnSide === 'Left') {
-            this.setState({isPressedLeft: true, isPressedRight: false, btnSide: btnSide});
+            this.setState({activeLeft: true, activeRight: false});
         } else if (btnSide === 'Right') {
-            this.setState({isPressedLeft: false, isPressedRight: true, btnSide: btnSide});
+            this.setState({activeLeft: false, activeRight: true});
         } else {
             return;
         }
     }
-    //     const pressedLeft = this.state.isPressedLeft;
-    //     // const pressedRight = this.state.isPressedRight;
-    //     pressedLeft ? this.setState({isPressedLeft: true, isPressedRight: false, btnSide: 'left'}) : this.setState({
-    //         isPressedLeft: false,
-    //         isPressedRight: true,
-    //         btnSide: 'right'
-    //     });
-
 
     render() {
-        const {btnSide, active} = this.state;
-        const btnClass = cn('btn', 'btn-secondary', {
-            [`${btnSide}`]: true,
-            'active': active
+        const {activeLeft, activeRight} = this.state;
+        const btnClassLeft = cn('btn', 'btn-secondary', 'left', {
+            'active': activeLeft
+        });
+        const btnClassRight = cn('btn', 'btn-secondary', 'right', {
+            'active': activeRight
         });
         return (
             <div onClick={this.btnLeftHandler} className="btn-group" role="group">
-                <button type="button" className={btnClass}>Left</button>
-                <button type="button" className={btnClass}>Right</button>
+                <button type="button" className={btnClassLeft}>Left</button>
+                <button type="button" className={btnClassRight}>Right</button>
             </div>
         )
     }
 }
 
-export default ButtonGroup;
+export default BtnGroup;
+
+// btnLeftHandler = (e) => () => {
+//     const target = e.target;
+//     const btnSide = target.innerText;
+//     if (btnSide === 'Left') {
+//         this.setState({isPressedLeft: true, isPressedRight: false, btnSide: btnSide});
+//     } else if (btnSide === 'Right') {
+//         this.setState({isPressedLeft: false, isPressedRight: true, btnSide: btnSide});
+//     } else {
+//         return;
+//     }
+// }
+//     const pressedLeft = this.state.isPressedLeft;
+//     // const pressedRight = this.state.isPressedRight;
+//     pressedLeft ? this.setState({isPressedLeft: true, isPressedRight: false, btnSide: 'left'}) : this.setState({
+//         isPressedLeft: false,
+//         isPressedRight: true,
+//         btnSide: 'right'
+//     });
+
+
+//     render() {
+//         const {btnSide, active} = this.state;
+//         const btnClass = cn('btn', 'btn-secondary', {
+//             [`${btnSide}`]: true,
+//             'active': active
+//         });
+//         return (
+//             <div onClick={this.btnLeftHandler} className="btn-group" role="group">
+//                 <button type="button" className={btnClass}>Left</button>
+//                 <button type="button" className={btnClass}>Right</button>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default ButtonGroup;
